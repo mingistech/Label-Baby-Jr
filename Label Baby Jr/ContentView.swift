9408d8696c85663c36783e1d2597fe112a963fba
Brandon Thompson <brandon.k.thompson@me.com>
2026-07-25 10:29:46 -0400

    Initial Commit

---
+++ Label Baby Jr/ContentView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+import Playgrounds
+
+@main struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+#Playground {
+    _ = 1 + 2
+}
+ 
